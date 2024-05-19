a470f1455c6486d9212f7da7e79d593cc45ad442
pnjia <133610889+pnjia@users.noreply.github.com>
2024-05-19 13:13:31 +0700

    Update alarm_clock.py

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,11 +1,11 @@
 def alarm_clock(day, vacation):
   if vacation:
-      if day in [0, 6]:  # Weekend
+      if day in [0, 6]:  
           return "off"
-      else:  # Weekday
+      else:  
           return "10:00"
   else:
-      if day in [0, 6]:  # Weekend
+      if day in [0, 6]: 
           return "10:00"
-      else:  # Weekday
+      else:  
           return "7:00"
